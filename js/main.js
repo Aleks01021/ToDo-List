0028d336d770c8a76e64cf2e165811801b607261
Aleks01021 <aleks_voronov1@mail.ru>
2016-10-15 23:46:38 +0300

    nextcommit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,36 @@
+var form  = document.forms.form,
+	check = form.elements.check,
+	year = form.elements.year,
+	texter = form.elements.texter,
+	month = form.elements.month,
+	numberMont = form.elements.number_mont,
+	day = form.elements.day,
+	sub = form.elements.submit;
 
-document.getElementByid('data_day').innerHTML = Date();
+sub.addEventListener('click',getData);
+function getData(){
+	var isChecked = check.checked,
+	content = texter.value,
+	yearContent = year.value,
+	monthContent = month.value,
+	numberMontContent = number_mont.value,
+	dayContent = day.value;
+	generationTodo({
+		check:isChecked,
+		texter:content,
+		year:yearContent,
+		month:monthContent,
+		numberMont:numberMontContent,
+		day:dayContent
+
+	});
+
+
+
+};
+function generationTodo(params){
+
+};
+
+
+
