ec724f0532e5ec1169f9df09fc10237bce801a1f
Aleks01021 <aleks_voronov1@mail.ru>
2016-10-16 13:43:34 +0300

    newcommit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Присвоение переменных
 var form  = document.forms.form,
 	check = form.elements.check,
 	year = form.elements.year,
@@ -5,9 +6,9 @@ var form  = document.forms.form,
 	month = form.elements.month,
 	numberMont = form.elements.number_mont,
 	day = form.elements.day,
-	sub = form.elements.submit;
-
-sub.addEventListener('click',getData);
+	btn = form.elements.btn;
+//
+btn.addEventListener('click',getData);
 function getData(){
 	var isChecked = check.checked,
 	content = texter.value,
