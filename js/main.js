0a3a00a5e902e70d750f7734c7f6283bdfb9fae5
Aleks01021 <aleks_voronov1@mail.ru>
2016-10-18 20:48:05 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,22 +1,65 @@
 //Присвоение переменных
-var form  = document.forms.form,
-	check = form.elements.check,
-	year = form.elements.year,
-	texter = form.elements.texter,
-	month = form.elements.month,
-	numberMont = form.elements.number_mont,
-	day = form.elements.day,
-	btn = form.elements.btn;
+//var form  = document.getElementById('#form');
+var check = document.getElementById('#check');
+var formControl = document.getElementById('form_control')
+var texter = document.getElementById('#texter');
+var todoList = document.getElementById('#todoList');
 
-btn.addEventListener('click',getData);
-function getData(){
+function newItem() {
+
+	//Checkbox
+	var isChecked = document.getElementById('check').value;
+	var check = document.createTextNode(isChecked);
+	var newItem1 = document.createElement("li");
+	newItem1.appendChild(check);
+	document.getElementById("todoList").appendChild(newItem1);
+	var ch = document.getElementById("check").checked;
+    document.getElementById("todoList").innerHTML = ch;
+	//Date
+
+	var dataControl = document.getElementById('form_control').value;
+	var formControl = document.createTextNode(dataControl);
+	var newItem2 = document.createElement('li');
+	newItem2.appendChild(formControl);
+	document.getElementById('todoList').appendChild(newItem2);
+
+
+
+	//Textarea
+	var item = document.getElementById("texter").value;
+	var texter = document.createTextNode(item);
+	var newItem3 = document.createElement("li");
+	newItem3.appendChild(texter);
+	document.getElementById("todoList").appendChild(newItem3);
+
+
+
+};
+
+
+$(function () {
+$('#datetimepicker2').datetimepicker(
+      {language: 'ru'}
+   );
+});
+
+
+
+
+
+
+
+
+
+
+/*function todoList(){
 	var isChecked = check.checked,
 	content = texter.value,
 	yearContent = year.value,
 	monthContent = month.value,
 	numberMontContent = number_mont.value,
 	dayContent = day.value;
-	generateTodo({
+	generationTodo({
 		check:isChecked,
 		texter:content,
 		year:yearContent,
@@ -33,13 +76,12 @@ function getData(){
 function generationTodo(params){
 
 };
-function todoList(){
+/*function todoList(){
 
-
-	/*var item = document.getElementByid('texter').value
-	var texter = document.createTextNode(item);
-	var newItem = document.createElement('li');
+	var item = document.getElementByid('texter').value
+	var texter = document.createTextNode(item)
+	var newItem = document.createElement('li')
 	newItem.appendChild(texter)
-	document.getElementById('todoList').appendChild(newItem)*/
+	document.getElementById('todoList').appendChild(newItem)
 };
-
+*/
