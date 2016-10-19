b32d47cb18fab58eca9d3a06652edd182f68528a
Aleks01021 <aleks_voronov1@mail.ru>
2016-10-19 21:41:18 +0300

    new

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ var todoList = document.getElementById('#todoList');
 function newItem() {
 
 //Checkbox
-	var isChecked = document.getElementById('check').value;
+	/*var isChecked = document.getElementById('check').value;
 	var check = document.createTextNode(isChecked);
 	var newItem1 = document.createElement("li");
 	newItem1.appendChild(check);
-	document.getElementById("todoList").appendChild(newItem1);
+	document.getElementById("todoList").appendChild(newItem1);*/
 
 	//Date
 	var dataControl = document.getElementById('form_control').value;
@@ -34,7 +34,16 @@ function newItem() {
 
 };
 
-
+$(document).ready(function(){
+        $('input[type="checkbox"]').click(function(){
+         if($(this).prop("checked") == true){
+           alert("Checkbox is checked.");
+         }
+         else if($(this).prop("checked") == false){
+           alert("Checkbox is unchecked.");
+         }
+     });
+ });
 
 $(function () {
 $('#datetimepicker2').datetimepicker(
@@ -54,36 +63,3 @@ $(document).ready(function(){
 
 
 
-/*function todoList(){
-	var isChecked = check.checked,
-	content = texter.value,
-	yearContent = year.value,
-	monthContent = month.value,
-	numberMontContent = number_mont.value,
-	dayContent = day.value;
-	generationTodo({
-		check:isChecked,
-		texter:content,
-		year:yearContent,
-		month:monthContent,
-		numberMont:numberMontContent,
-		day:dayContent
-
-
-	});
-
-
-
-};
-function generationTodo(params){
-
-};
-/*function todoList(){
-
-	var item = document.getElementByid('texter').value
-	var texter = document.createTextNode(item)
-	var newItem = document.createElement('li')
-	newItem.appendChild(texter)
-	document.getElementById('todoList').appendChild(newItem)
-};
-*/
